10d7494713963b99ac7aa5f893bbd9445d6a6bb5
Ashish <akuzi770@gmail.com>
2019-02-01 18:10:33 +0530

    added swipe

--- Script/Main.gd
+++ Script/Main.gd
@@ -1,7 +1,7 @@
 extends Node
 
-export(float) var x_start = 0.0
-export(float) var y_start = 0.0
+export(float) var x_margin = 0
+export(float) var y_margin = 0
 
 const N = 1
 const E = 2
@@ -15,25 +15,47 @@ var tile_size = 64  # tile size (in pixels)
 var width = 30  # width of map (in tiles)
 var height = 40  # height of map (in tiles)
 var scr_size = OS.window_size
-var curr_touch_pos = Vector2(0, 0)
-var prev_touch_pos = Vector2(-1, 0)
+
+var curr_touch_grid = Vector2(0, 0)
+var prev_touch_grid = Vector2(0, 0)
+var base_touch_pos = Vector2(1080/2, 1920/2)
+var curr_touch_pos
+var curr_swipe_dir
+
 var line_points = []
+var cool_down_timer
+var can_draw = true
 
 # get a reference to the map for convenience
 onready var Map = $TileMap
 onready var Line = $Line2D
 
+#+++++++++++++++++++++++++ READY AND PROCESS +++++++++++++++++++++++++
+
 func _ready():
 	randomize()
+	
+	cool_down_timer = Timer.new()
+	cool_down_timer.set_one_shot(true)
+	cool_down_timer.set_wait_time(0.5)
+	cool_down_timer.connect("timeout", self, "on_timeout_complete")
+	add_child(cool_down_timer)
+	
 	print(OS.window_size)
 	tile_size = Map.cell_size
 	
-	width = floor(1080 / tile_size.x)
-	height = floor(1920 / tile_size.y)
+	width = floor((1080 - 2 * x_margin) / tile_size.x)
+	height = floor((1920 - 2 * y_margin) / tile_size.y)
 	
 	print(String(width)+" "+String(height))
-	
+	#centre the map
+	Map.position = Vector2(x_margin/2, y_margin/2)
 	make_maze()
+
+func _process(delta):
+	touch_input()
+
+#+++++++++++++++++++++++++ MAZE GENRATION +++++++++++++++++++++++++
 
 func check_neighbors(cell, unvisited):
 	# returns an array of cell's unvisited neighbors
@@ -72,44 +94,92 @@ func make_maze():
 			current = stack.pop_back()
 		#yield(get_tree(), "idle_frame")
 		
-		#set first open
-		Map.set_cellv( Vector2(0, 0), Map.get_cellv(Vector2(0, 0)) - 8)
+		#set first and last open
+		Map.set_cellv( Vector2(0, 0), Map.get_cellv(Vector2(0, 0)) - W)
+		Map.set_cellv( Vector2(width-1, height-1), Map.get_cellv(Vector2(width-1, height-1)) - E)
+
+#+++++++++++++++++++++++++ HELPER +++++++++++++++++++++++++
 
 func pixel_to_grid(pixel_cord):
-	var new_x = floor((pixel_cord.x - x_start)/tile_size.x)
-	var new_y = floor((pixel_cord.y - y_start)/tile_size.y)
+	var new_x = floor((pixel_cord.x - x_margin)/tile_size.x)
+	var new_y = floor((pixel_cord.y - y_margin)/tile_size.y)
 	#print(String(new_x) + " " + String(new_y))
 	return Vector2(new_x, new_y)
 
 func grid_to_pixel(grid_cord):
-	var new_x = x_start + grid_cord.x * tile_size.x + tile_size.x/2
-	var new_y = y_start + grid_cord.y * tile_size.y + tile_size.y/2
-	#print(String(new_x) + " " + String(new_y))
+	var new_x = x_margin/2 + grid_cord.x * tile_size.x + tile_size.x/2
+	var new_y = y_margin/2 + grid_cord.y * tile_size.y + tile_size.y/2
+	#print("pg: "+String(new_x) + " " + String(new_y))
 	return Vector2(new_x, new_y)
 
+func can_move():
+	if(cell_walls.has(curr_touch_grid - prev_touch_grid)):
+		#check if valid move
+		var dir = prev_touch_grid - curr_touch_grid
+		if(Map.get_cellv(curr_touch_grid) & cell_walls[dir]):
+			print("*Cant move")
+			return false
+	return true
+
+func on_timeout_complete():
+	can_draw = true
+
+#+++++++++++++++++++++++++ TOUCH MANAGE +++++++++++++++++++++++++
+
 func touch_input():
-	if Input.is_action_pressed("ui_click"):
-		curr_touch_pos = pixel_to_grid(get_viewport().get_mouse_position())
-		draw_line(curr_touch_pos)
-		#print("point added at: "+ String(grid_to_pixel(curr_touch_pos)))
+	if Input.is_action_just_pressed("ui_click") and can_draw:
+		 base_touch_pos = get_viewport().get_mouse_position()
+	
+	if Input.is_action_pressed("ui_click") and can_draw:
+		
+		#curr_touch_grid = pixel_to_grid(get_viewport().get_mouse_position())
+		#curr_swipe_dir  = get_swipe_dir()
+		if(can_move()):
+			curr_touch_grid +=  get_swipe_dir()
+			print(curr_touch_grid)
+			draw_line()
+		
+		can_draw = false
+		cool_down_timer.start()
+		#print("point added at: "+ String(grid_to_pixel(curr_touch_grid)))
 		#pixel_to_grid(get_global_mouse_position())
 
-func draw_line(curr_touch_pos):
-	if(line_points.size()>2 and curr_touch_pos == line_points[line_points.size()-2]):
+func draw_line():
+	#if go back remove line point
+	if(line_points.size() > 2 and curr_touch_grid == line_points[line_points.size()-2]):
 		Line.remove_point(line_points.size()-1)
 		line_points.pop_back()
 	
-	if(cell_walls.has(curr_touch_pos - prev_touch_pos)):
+	print("can move: "+ String(can_move()))
+	
+	if(cell_walls.has(curr_touch_grid - prev_touch_grid)):
 		#check if valid move
-		var dir = prev_touch_pos - curr_touch_pos
-		if(Map.get_cellv(curr_touch_pos) & cell_walls[dir]):
+		var dir = prev_touch_grid - curr_touch_grid
+		if(Map.get_cellv(curr_touch_grid) & cell_walls[dir]):
 			print("*Cant move")
 			return
 		
-		prev_touch_pos = curr_touch_pos
-		Line.add_point(grid_to_pixel(curr_touch_pos))
-		line_points.append(curr_touch_pos)
-		print("point added at: "+ String(grid_to_pixel(curr_touch_pos)))
+		prev_touch_grid = curr_touch_grid
+		Line.add_point(grid_to_pixel(curr_touch_grid))
+		line_points.append(curr_touch_grid)
+		#print("point added at: "+ String(grid_to_pixel(curr_touch_grid)))
 
-func _process(delta):
-	touch_input()
+func get_swipe_dir():
+	curr_touch_pos = get_viewport().get_mouse_position()
+	var dir = Vector2 (0, 0)
+	if(curr_touch_pos != base_touch_pos):
+		dir = curr_touch_pos - base_touch_pos
+		if(abs(dir.x)> abs(dir.y)):
+			dir.y = 0
+			if(dir.x > 0):
+				dir.x = 1
+			else:
+				dir.x = -1
+		else:
+			dir.x = 0
+			if(dir.y > 0):
+				dir.y = 1
+			else:
+				dir.y = -1	
+	print(dir)
+	return dir
