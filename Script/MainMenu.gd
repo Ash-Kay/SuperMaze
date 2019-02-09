2168aa045d8c9b1c2b24184580e2368c0ae6917b
ASHISH <akuzi770@gmail.com>
2019-02-10 00:41:09 +0530

    UI minor improvement..., add SOUND

--- Script/MainMenu.gd
+++ Script/MainMenu.gd
@@ -7,18 +7,8 @@ func _ready():
 	GameScene = "res://Scene/Game.tscn"
 	SettingPopup = get_node("UI/Popup")
 
-
 func _on_play_pressed():
 	get_tree().change_scene(GameScene)
-
-
-func _on_credits_pressed():
-	pass # replace with function body
-
-
-func _on_AD_pressed():
-	pass # replace with function body
-
 
 func _on_settings_pressed():
 	SettingPopup.popup()
